e901ff1c6516a6db86a388b74988fb7de3fa8a44
Oskari Seppä <seppaoskari@gmail.com>
2023-09-14 19:39:29 +0300

    Added OUT endpoint to USB HID

--- app/src/usb_hid.c
+++ app/src/usb_hid.c
@@ -15,16 +15,46 @@
 #include <zmk/keymap.h>
 #include <zmk/event_manager.h>
 
+#if IS_ENABLED(CONFIG_BOARDPILOT)
+#include <zmk/boardpilot/boardpilot.h>
+#endif
+
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static const struct device *hid_dev;
+
+#if IS_ENABLED(CONFIG_ZMK_BOARDPILOT)
+static uint8_t bp_message_buffer[64];
+static uint32_t bp_message_len = 0;
+#endif
 
 static K_SEM_DEFINE(hid_sem, 1, 1);
 
 static void in_ready_cb(const struct device *dev) { k_sem_give(&hid_sem); }
 
-static const struct hid_ops ops = {
-    .int_in_ready = in_ready_cb,
+#if IS_ENABLED(CONFIG_ZMK_BOARDPILOT)
+static void out_ready_cb(const struct device *dev) {
+
+    memset(bp_message_buffer, 0, 64);
+
+    int err = hid_int_ep_read(dev, bp_message_buffer, 64, &bp_message_len);
+    if (err < 0) {
+        // Failed to read
+        return;
+    }
+
+    err = zmk_boardpilot_control_parse(bp_message_buffer, bp_message_len);
+    if (err < 0) {
+        // Failed to parse
+        return;
+    }
+}
+#endif
+
+static const struct hid_ops ops = {.int_in_ready = in_ready_cb,
+#ifdef CONFIG_ENABLE_HID_INT_OUT_EP
+                                   .int_out_ready = out_ready_cb
+#endif
 };
 
 int zmk_usb_hid_send_report(const uint8_t *report, size_t len) {
