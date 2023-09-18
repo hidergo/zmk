925bd16af0270386eb1b2a419c3f881c57432e42
Oskari Seppä <seppaoskari@gmail.com>
2023-09-18 21:58:47 +0300

    Fixed incorrect preprocessor directive

--- app/src/keymap.c
+++ app/src/keymap.c
@@ -26,7 +26,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/events/layer_state_changed.h>
 #include <zmk/events/sensor_event.h>
 
-#if IS_ENABLED(ZMK_BOARDPILOT_KEYMAP)
+#if IS_ENABLED(CONFIG_ZMK_BOARDPILOT_KEYMAP)
 #include <zmk/boardpilot/boardpilot.h>
 #endif
 
